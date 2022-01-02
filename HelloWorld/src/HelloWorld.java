55e42658545877a4e26fab7ecef167a91a994719
jonat <jonat@DESKTOP-66ISP16.attlocal.net>
2022-01-02 14:22:06 -0600

    Deleted Comments

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,18 +3,5 @@ public class HelloWorld {
 
 	public static void main(String[]args) {
 		System.out.println("Hello World");
-		
-	/*	
-		 double price = 90000;
-         String model;
-         if(price > 100000) {
-             model = "Tesla Model X";
-             System.out.println(model);
-         } else if(price <= 100000) {
-             model = "Tesla Model S";
-             System.out.println(model);
-         }
-          */
-		
 	}
 }
